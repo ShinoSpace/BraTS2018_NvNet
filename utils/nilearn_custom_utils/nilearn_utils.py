2978a5bcc0f4a838bbe0eeda4f96c0001e07eb82
ShinoSpace <22547794+ShinoSpace@users.noreply.github.com>
2020-09-12 01:31:59 +0800

    Update nilearn_utils.py

--- utils/nilearn_custom_utils/nilearn_utils.py
+++ utils/nilearn_custom_utils/nilearn_utils.py
@@ -38,17 +38,19 @@ def crop_img(img, rtol=1e-8, copy=True, return_slices=False):
     if data.ndim == 4:  # data: ndarray, ndim是看data总共有几维，即data.ndim == len(data.shape)
         # 4D图像，沿着最后一维（各行横向遍历）检查是否有true. 这部分目的是检查每行是否有前景像素。
         passes_threshold = np.any(passes_threshold, axis=-1)    # check沿着指定维是否有true.
-    coords = np.array(np.where(passes_threshold))   # 拿到前景像素的坐标/索引
-    start = coords.min(axis=1)      # 对非零像素坐标，分别取各维的最小值
-    end = coords.max(axis=1) + 1    # 对非零像素坐标，分别取各维的最大值
+    coords = np.array(np.where(passes_threshold))   # 拿到前景像素的坐标/索引，是一个二维矩阵，coords[0], coords[1], coords[2]都是一个数组，数组内元素是axis=0, 1, 2的索引。
+    start = coords.min(axis=1)      # 对非零像素坐标，分别取各维的最小值。结果是一个包含3个值的vector, start[0]是非零像素索引中，axis=0的索引最小值。start[1], start[2]同理。
+    end = coords.max(axis=1) + 1    # 对非零像素坐标，分别取各维的最大值。结果意义与上一行同理。
 
     # pad with one voxel to avoid resampling problems
-    # 下面这两行是在进行越界检查
-    start = np.maximum(start - 1, 0)    # 标量0会广播地与start-1比较，取最大
+    # 我们想保留图像非零区域，并额外保留一圈零像素。下面这两行是在进行越界检查。
+    start = np.maximum(start - 1, 0)    # 标量0会广播地与start-1比较，取最大。例如np.maximum([1,2,3], 5) → array([5, 5, 5])。下同。
     end = np.minimum(end + 1, data.shape[:3])
-
-    slices = [slice(s, e) for s, e in zip(start, end)]
+    
+    # 有点乱，举个例子：根据上面的代码及含义，start的三个元素为非零像素点在axis=0, 1, 2上的最小值，不妨设start=[0, 1, 0]
+    # 同理，设end=[2, 2, 2]. 于是zip(start, end) = [(0,2), (1,2), (0,2)].
+    slices = [slice(s, e) for s, e in zip(start, end)]  # slices = [slice(0,2), slice(1,2), slice(0,2)]. 注：slice(s, e)为切片对象，用其作索引相当于使用切片语法[s:e].
     if return_slices:
         return slices
 
-    return crop_img_to(img, slices, copy=copy)
+    return crop_img_to(img, slices, copy=copy)  # 似乎是根据给定的切片列表slices, 对img进行切片？
